d58d34dcdbd3be48e2dca1e4edfab2d51f043d22
shawjia <shawjia@me.com>
2018-10-20 19:13:57 +0800

    ✨ add fund finished

--- src/app/pages/index/index.js
+++ src/app/pages/index/index.js
@@ -3,12 +3,48 @@
 const app = getApp();
 
 Page({
+  data: {
+    funds: [],
+
+    showAdd: false,
+  },
+
+  onLoad() {
+    const funds = wx.getStorageSync('funds') || [];
+
+    this.setData({
+      funds,
+      showAdd: funds.length === 0,
+    });
+  },
 
   onShow() {
     console.log('ohShow', app.globalData);
   },
 
-  data: {
+  handleAdd(e) {
+    const { funds } = this.data;
+    const {
+      code, amount, price, from,
+    } = e.detail.value;
+
+    // TODO: check input
+
+    const newFunds = [...funds, {
+      code, amount, price, from, add: Date.now(),
+    }];
+
+    this.setData({ funds: newFunds, showAdd: false });
+
+    wx.setStorageSync('funds', newFunds);
+  },
+
+  toggleShowAdd() {
+    const { showAdd } = this.data;
+
+    this.setData({
+      showAdd: !showAdd,
+    });
   },
 
 });
